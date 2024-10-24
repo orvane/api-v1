473014fc1666702d10638074d035394bccea302f
dawidseipold <seipold.dawid@gmail.com>
2024-10-24 23:54:30 +0200

    feat: add a basic tcp server and add a signup route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+use axum::{routing::post, Router};
+use routes::auth::signup::user_signup;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/api/v1/auth/signup", post(user_signup));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap()
 }
