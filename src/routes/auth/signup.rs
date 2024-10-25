349584eb5b82330e324f5378bfd31e21051cff8f
dawidseipold <seipold.dawid@gmail.com>
2024-10-25 22:54:06 +0200

    feat(auth): add the validation and user_input to the signup route
    Also add more descriptive error handling that uses the used functions
    error messages in the response

--- src/routes/auth/signup.rs
+++ src/routes/auth/signup.rs
@@ -3,10 +3,26 @@ use axum::{
     Json,
 };
 use hyper::StatusCode;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use uuid::Uuid;
-use validator::Validate;
+use validator::{Validate, ValidationErrors};
+
+#[derive(Debug, Deserialize, Validate)]
+struct EmailInput {
+    #[validate(email)]
+    email: String,
+}
+
+// #[derive(Debug, Deserialize, Validate)]
+// struct PasswordInput {
+//     password: String,
+// }
+
+#[derive(Debug, Deserialize, Validate)]
+pub struct SignUpRequest {
+    email: String,
+}
 
 #[derive(Serialize, Validate)]
 pub struct User {
@@ -15,7 +31,22 @@ pub struct User {
     email: String,
 }
 
+async fn validate_email_input(email: &str) -> Result<(), ValidationErrors> {
+    let email_input = EmailInput {
+        email: email.to_string(),
+    };
+
+    match email_input.validate() {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e),
+    }
+}
+
 async fn check_if_user_exists(email: &str) -> Result<bool, String> {
+    validate_email_input(email)
+        .await
+        .map_err(|_| "Invalid email format".to_string())?;
+
     // Some logic to call a database, throw errors if any, validate input and throw validation
     // errors if there are any
     let db_dummy_data = vec![
@@ -31,21 +62,29 @@ async fn check_if_user_exists(email: &str) -> Result<bool, String> {
     }
 }
 
-pub async fn user_signup() -> Result<Response, StatusCode> {
-    let user_exists = check_if_user_exists("john@doe.com").await;
+pub async fn user_signup(Json(payload): Json<SignUpRequest>) -> Result<Response, StatusCode> {
+    if let Err(e) = validate_email_input(&payload.email).await {
+        let response_body = Json(json!({"message": "Bad request", "data": e.to_string()}));
 
-    match user_exists {
+        return Ok((StatusCode::BAD_REQUEST, response_body).into_response());
+    }
+
+    match check_if_user_exists(&payload.email).await {
+        Ok(false) => {}
         Ok(true) => {
             let response_body = Json(json!({"message": "User already exists"}));
-            Ok((StatusCode::CONFLICT, response_body).into_response())
+
+            return Ok((StatusCode::CONFLICT, response_body).into_response());
         }
-        Ok(false) => {
-            let response_body = Json(json!({"message": "Email available"}));
-            Ok((StatusCode::CREATED, response_body).into_response())
-        }
-        Err(_) => {
-            let response_body = Json(json!({"message": "An error occured"}));
-            Ok((StatusCode::INTERNAL_SERVER_ERROR, response_body).into_response())
+        Err(e) => {
+            let response_body =
+                Json(json!({"message": "Internal server error", "data": e.to_string()}));
+
+            return Ok((StatusCode::INTERNAL_SERVER_ERROR, response_body).into_response());
         }
     }
+
+    let response_body = Json(json!({"message": "User created successfully"}));
+
+    Ok((StatusCode::CREATED, response_body).into_response())
 }
