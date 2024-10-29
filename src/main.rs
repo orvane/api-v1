1787dcb0227e52d770104542abbbf5de241b0618
dawidseipold <seipold.dawid@gmail.com>
2024-10-29 02:23:39 +0100

    feat: add email middleware to the main route

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod routes;
+mod services;
 
-use axum::{routing::post, Router};
+use axum::{routing::post, Extension, Router};
+use dotenv::dotenv;
 use routes::auth::signup::{self};
+use services::email_service::EmailLayer;
+use std::env;
 use surrealdb::{
     engine::remote::ws::{Client, Ws},
     opt::auth::Root,
@@ -15,6 +19,8 @@ struct AppState {
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
+    dotenv().ok();
+
     let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
     db.signin(Root {
         username: "root",
@@ -25,8 +31,17 @@ async fn main() -> surrealdb::Result<()> {
 
     let shared_state = AppState { db };
 
+    let email_layer = EmailLayer::new(
+        env::var("RESEND_API_KEY").unwrap_or_else(|_| {
+            println!("Resend API key error");
+            String::new()
+        }),
+        String::from("blazar.lol"),
+    );
+
     let app = Router::new()
         .route("/api/v1/auth/signup", post(signup::signup))
+        .layer(Extension(email_layer))
         .with_state(shared_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
