d669a3ec3b16f342b1e35a61ac9f208fa49ade74
dawidseipold <seipold.dawid@gmail.com>
2024-10-31 03:27:34 +0100

    feat(auth): add a security constraint for email verification creations, move its whole to a function

--- src/routes/auth/signup.rs
+++ src/routes/auth/signup.rs
@@ -11,9 +11,10 @@ use axum::{
 use derive_more::Display;
 use hyper::StatusCode;
 use rand::{thread_rng, Rng};
+use resend_rs::types::ErrorResponse;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-use surrealdb::{engine::remote::ws::Client, Surreal};
+use surrealdb::{engine::remote::ws::Client, sql::Uuid, Surreal};
 use validator::Validate;
 
 use crate::{services::email_service::EmailLayer, AppState};
@@ -27,11 +28,56 @@ struct EmailInput {
 #[derive(Serialize, Deserialize, Validate, Debug, Clone)]
 pub struct EmailVerification {
     code: String,
+    email: String,
 }
 
 impl EmailVerification {
-    pub fn new(code: String) -> EmailVerification {
-        EmailVerification { code }
+    pub async fn new(
+        code: String,
+        email: String,
+        db: &Surreal<Client>,
+    ) -> Result<EmailVerification, surrealdb::Error> {
+        let id = Uuid::new_v4().to_string();
+
+        let check_query = r#"
+            SELECT * FROM email_verification
+            WHERE email = $email
+        "#;
+
+        let mut check = db.query(check_query).bind(("email", email.clone())).await?;
+
+        let email_verifications: Vec<EmailVerification> = check.take(0)?;
+
+        if !email_verifications.is_empty() {
+            let delete_query = r#"
+                DELETE FROM email_verification
+                WHERE email = $email
+            "#;
+
+            db.query(delete_query)
+                .bind(("email", email.clone()))
+                .await?;
+        }
+
+        let create_query = r#"
+            CREATE email_verification
+            SET
+                id = $id,
+                email = $email,
+                code = $code,
+                created_ay = time::now()
+        "#;
+
+        let mut result = db
+            .query(create_query)
+            .bind(("id", id))
+            .bind(("email", email))
+            .bind(("code", code))
+            .await?;
+
+        let created: Vec<EmailVerification> = result.take(0)?;
+
+        Ok(created.into_iter().next().unwrap())
     }
 }
 
@@ -63,16 +109,20 @@ pub enum SignupError {
     DatabaseError(surrealdb::Error),
     EmailError(String),
     HashingError,
+    OtherError(String),
 }
 
 impl IntoResponse for SignupError {
     fn into_response(self) -> Response {
         let status_code = match &self {
+            // TODO: Make these errors more descriptive (also make them use their actual message)
+            // TODO: Move errors to a different folder
             SignupError::ValidationError => StatusCode::BAD_REQUEST,
             SignupError::DatabaseError(_) => StatusCode::INTERNAL_SERVER_ERROR,
             SignupError::EmailUnavailableError => StatusCode::BAD_REQUEST,
             SignupError::EmailError(_) => StatusCode::BAD_REQUEST,
             SignupError::HashingError => StatusCode::INTERNAL_SERVER_ERROR,
+            SignupError::OtherError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         };
 
         let body = Json(serde_json::json!({"error": self.to_string()}));
@@ -142,6 +192,13 @@ pub fn verify_string_hash(input: String, hash: String) -> bool {
     hash == new_hash
 }
 
+// TODO: In case if any of the steps fails the previous steps need to be reverted
+// For example: If user gets created, but email-verification creation fails it needs to reverse the
+// user creation and throw the INTERNAL_SERVER_ERROR
+// The whole process should be handled manually since even if the email doesn't get sent to the
+// user the user record should still stay in the database as the email-verification request can be
+// created at any time
+// TODO: Clean up match statements (remove matches on Result and Option when possible)
 #[axum::debug_handler]
 pub async fn signup(
     State(app_state): State<AppState>,
@@ -180,22 +237,19 @@ pub async fn signup(
 
             let verification_code_hash = string_hash(verification_code.clone());
 
-            let _: Option<EmailVerification> = app_state
-                .db
-                .create(("email_verification", &payload.email))
-                .content(EmailVerification {
-                    code: verification_code_hash,
-                })
-                .await
-                .map_err(SignupError::DatabaseError)?;
+            let email_verification =
+                EmailVerification::new(verification_code_hash, email.clone(), &app_state.db).await;
 
-            // TODO: add a service that would have a function that generates a code like below,
-            // might combine it with the hashing service and name it utils or something
-            if let Err(e) = email_layer
-                .send_email_verification(email.clone(), verification_code)
-                .await
-            {
-                return Err(SignupError::EmailError(e));
+            match email_verification {
+                Ok(_) => {
+                    if let Err(e) = email_layer
+                        .send_email_verification(email.clone(), verification_code)
+                        .await
+                    {
+                        return Err(SignupError::EmailError(e));
+                    }
+                }
+                Err(e) => return Err(SignupError::DatabaseError(e)),
             }
 
             // TODO: Once the route handler is fully complete remove this response and make it more
