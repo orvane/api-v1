6441f3666626419223483bc3a3f4974782e1ad3d
dawidseipold <seipold.dawid@gmail.com>
2024-11-10 00:40:11 +0100

    refactor(auth): rewrite signup route handler

--- src/routes/auth/signup.rs
+++ src/routes/auth/signup.rs
@@ -1,7 +1,7 @@
 use axum::{extract::State, Extension, Json};
 
 use hyper::StatusCode;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use validator::Validate;
 
 use crate::{
@@ -11,16 +11,23 @@ use crate::{
         email::EmailLayer,
     },
     utils::{
-        crypto::{hash_password, string_hash},
+        crypto::{hash_password, hash_string},
         random::generate_random_code,
     },
     AppState,
 };
 
 #[derive(Debug, Deserialize, Validate)]
-struct EmailInput {
+pub struct RoutePayload {
     #[validate(email)]
     email: String,
+    // TODO: custom password validation functions
+    password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RouteOutput {
+    message: String,
 }
 
 // TODO: In case if any of the steps fails the previous steps need to be reverted
@@ -35,77 +42,87 @@ pub async fn signup(
     State(app_state): State<AppState>,
     Extension(database_layer): Extension<DatabaseLayer>,
     Extension(email_layer): Extension<EmailLayer>,
-    Json(payload): Json<User>,
-) -> Result<(StatusCode, Json<User>), SignupError> {
-    let email = payload.email.clone();
-    let email_input = EmailInput {
-        email: email.clone(),
+    Json(payload): Json<RoutePayload>,
+) -> Result<(StatusCode, Json<RouteOutput>), SignupError> {
+    // 1. Validate payload input
+    let payload_instance = RoutePayload {
+        email: payload.email.clone(),
+        password: payload.password.clone(),
     };
 
-    if email_input.validate().is_err() {
-        return Err(SignupError::ValidationError);
+    match payload_instance.validate() {
+        Ok(_) => println!("Validation passed successfully!"),
+        Err(e) => return Err(SignupError::ValidationError(e)),
     }
 
-    let password_hash = hash_password(String::from(payload.password))
-        .await
-        .map_err(|_| SignupError::HashingError)?;
+    // 2. Check if the email is available
 
-    let email_available = database_layer
+    let check_if_exists = database_layer
         .query()
         .user
-        .check_if_exists(email.clone())
+        .check_if_exists(payload.email.clone())
         .await;
 
-    match email_available {
-        Ok(false) => return Err(SignupError::EmailUnavailableError),
-        Ok(true) => {
-            let new_user_result = database_layer
-                .query()
-                .user
-                .create(payload.email.clone(), password_hash)
-                .await;
-
-            let new_user = match new_user_result {
-                Ok(Some(user)) => user,
-                Ok(None) => return Err(SignupError::OtherError("Error".to_string())),
-                Err(e) => return Err(SignupError::DatabaseError(e)),
-            };
-
-            //TODO: create an email_verification in a database (new table)
-            let verification_code = generate_random_code(6);
-
-            let verification_code_hash = string_hash(verification_code.clone());
-
-            let new_email_verification = database_layer
-                .query()
-                .email_verification
-                .create(verification_code_hash, email.clone())
-                .await;
-
-            match new_email_verification {
-                Ok(_) => {
-                    if let Err(e) = email_layer
-                        .send_email_verification(email.clone(), verification_code)
-                        .await
-                    {
-                        return Err(SignupError::EmailError(e));
-                    }
-                }
-                Err(e) => return Err(SignupError::DatabaseError(e)),
-            }
-
-            // TODO: Once the route handler is fully complete remove this response and make it more
-            // generic (so it doesn't expose any data)
-            // TODO: As a response return a session that is unauthorized
-            Ok((
-                StatusCode::CREATED,
-                Json(User {
-                    email: "whatever".to_string(),
-                    password: "whatever".to_string(),
-                    email_verified: false,
-                }),
-            ))
-        }
-        Err(e) => Err(SignupError::DatabaseError(e)),
+    match check_if_exists {
+        Ok(_) => println!("Email availability check completed successfully!"),
+        // TODO: Add a custom error in case the email is already taken
+        Err(e) => return Err(SignupError::DatabaseError(e)),
     }
+
+    // 3. Create a new user in the database
+
+    let password_hash = hash_password(payload.password.clone()).await;
+
+    let password_hash = match password_hash {
+        Ok(hash) => {
+            println!("Password hashed successfully!");
+            hash
+        }
+        Err(e) => return Err(SignupError::HashingError(e)),
+    };
+
+    let create_new_user = database_layer
+        .query()
+        .user
+        .create(payload.email.clone(), password_hash)
+        .await;
+
+    match create_new_user {
+        Ok(_) => println!("User created successfully!"),
+        Err(e) => return Err(SignupError::DatabaseError(e)),
+    }
+
+    // 4. Create email verification in the database
+
+    let verification_code = generate_random_code(6);
+    let verification_code_hash = hash_string(verification_code.clone());
+
+    let create_new_email_verification = database_layer
+        .query()
+        .email_verification
+        .create(verification_code_hash, payload.email.clone())
+        .await;
+
+    match create_new_email_verification {
+        Ok(_) => println!("Email verification created successfully!"),
+        Err(e) => return Err(SignupError::DatabaseError(e)),
+    }
+
+    // 5. Send email verification email
+
+    let send_email_verification_email = email_layer
+        .send_email_verification(payload.email, verification_code)
+        .await;
+
+    match send_email_verification_email {
+        Ok(_) => println!("Email verification email sent successfully!"),
+        Err(e) => return Err(SignupError::EmailServiceError(e)),
+    }
+
+    Ok((
+        StatusCode::OK,
+        Json(RouteOutput {
+            message: String::from("Signup completed successfully!"),
+        }),
+    ))
 }
