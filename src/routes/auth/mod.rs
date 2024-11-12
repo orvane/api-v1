c8f8598e8d109f8b0e5ff1f79b50bae9bca3e93f
dawidseipold <seipold.dawid@gmail.com>
2024-11-12 03:34:33 +0100

    feat(auth): add a new auth router

--- src/routes/auth/mod.rs
+++ src/routes/auth/mod.rs
@@ -1,3 +1,17 @@
 pub mod email_verification;
 pub mod password_reset_request;
 pub mod signup;
+
+use axum::{routing::post, Router};
+pub use email_verification::email_verification;
+pub use password_reset_request::password_reset_request;
+pub use signup::signup;
+
+use crate::setup::AppState;
+
+pub fn auth_router() -> Router<AppState> {
+    Router::new()
+        .route("/signup", post(signup))
+        .route("/email-verification", post(email_verification))
+        .route("/password-reset-request", post(password_reset_request))
+}
