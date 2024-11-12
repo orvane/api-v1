9558007431656ff8099aa7b0221f7f795d02fd3f
dawidseipold <seipold.dawid@gmail.com>
2024-11-12 03:35:11 +0100

    feat: add a main_router and api_v1_router

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,1 +1,14 @@
 pub mod auth;
+
+use axum::Router;
+
+use crate::setup::AppState;
+
+fn api_v1_router() -> Router<AppState> {
+    Router::new().nest("/auth", auth::auth_router())
+}
+
+// Main router that serves as the entry point for all routes
+pub fn main_router() -> Router<AppState> {
+    Router::new().nest("/api/v1", api_v1_router())
+}
