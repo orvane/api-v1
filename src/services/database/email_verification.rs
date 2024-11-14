fa18c865a90f539ae8d0981ab77b33ed9b050532
dawidseipold <seipold.dawid@gmail.com>
2024-11-14 05:06:32 +0100

    chore(middleware): adjust email_verification to copy aproaches from other database services (Thing, query)

--- src/services/database/email_verification.rs
+++ src/services/database/email_verification.rs
@@ -1,16 +1,41 @@
+use chrono::{DateTime, Duration, Utc};
 use serde::{Deserialize, Serialize};
-use surrealdb::{engine::remote::ws::Client, sql::Uuid, Surreal};
+use surrealdb::{
+    engine::remote::ws::Client,
+    sql::{Datetime, Thing},
+    Surreal,
+};
 use validator::Validate;
+
+use crate::utils::crypto::generate_token;
 
 #[derive(Serialize, Deserialize, Validate, Debug, Clone)]
 pub struct EmailVerification {
+    pub id: Thing,
     pub code: String,
     pub email: String,
+
+    #[serde(default)]
+    pub created_at: Datetime,
+    #[serde(default)]
+    expires_at: Datetime,
 }
 
 impl EmailVerification {
-    pub fn new(email: String, code: String) -> Self {
-        EmailVerification { email, code }
+    pub fn new(
+        id: Thing,
+        email: String,
+        code: String,
+        created_at: Datetime,
+        expires_at: Datetime,
+    ) -> Self {
+        EmailVerification {
+            id,
+            email,
+            code,
+            created_at,
+            expires_at,
+        }
     }
 }
 
@@ -26,14 +51,24 @@ impl<'a> EmailVerificationQuery<'a> {
 }
 
 impl<'a> EmailVerificationQuery<'a> {
+    // TODO: This function needs better error exception handling, take a look at it in free time
     pub async fn create(
         &self,
         code: String,
         email: String,
+        user_id: Thing,
     ) -> Result<EmailVerification, surrealdb::Error> {
-        let new_email_verification = EmailVerification::new(code.clone(), email.clone());
+        let email_verification_id_str = generate_token();
+        let email_verification_id = Thing::from((
+            "email_verification".to_string(),
+            email_verification_id_str.clone(),
+        ));
 
-        let id = Uuid::new_v4().to_string();
+        let now: DateTime<Utc> = Utc::now();
+        let expires: DateTime<Utc> = now + Duration::minutes(5);
+
+        let created_at = Datetime::from(now);
+        let expires_at = Datetime::from(expires);
 
         let check_query = r#"
             SELECT * FROM email_verification
@@ -43,7 +78,7 @@ impl<'a> EmailVerificationQuery<'a> {
         let mut check = self
             .db
             .query(check_query)
-            .bind(("email", new_email_verification.email.clone()))
+            .bind(("email", email.clone()))
             .await?;
 
         let email_verifications: Vec<EmailVerification> = check.take(0)?;
@@ -56,50 +91,64 @@ impl<'a> EmailVerificationQuery<'a> {
 
             self.db
                 .query(delete_query)
-                .bind(("email", new_email_verification.email.clone()))
+                .bind(("email", email.clone()))
                 .await?;
         }
 
         let create_query = r#"
-            CREATE email_verification
-            SET
+            BEGIN TRANSACTION;
+
+            CREATE type::thing("email_verification", $id) SET
                 id = $id,
                 email = $email,
                 code = $code,
-                created_at = time::now()
+                created_at = $created_at,
+                expires_at = $expires_at;
+
+            RELATE $user_id -> email_verification -> $id;
+
+            COMMIT TRANSACTION;
         "#;
 
-        let mut result = self
-            .db
+        self.db
             .query(create_query)
-            .bind(("id", id))
-            .bind(("email", new_email_verification.email))
-            .bind(("code", new_email_verification.code))
+            .bind(("id", email_verification_id.clone()))
+            .bind(("email", email.clone()))
+            .bind(("code", code.clone()))
+            .bind(("user_id", user_id.clone()))
+            .bind(("expires_at", expires_at.clone()))
+            .bind(("created_at", created_at.clone()))
             .await?;
 
-        let email_verification: Vec<EmailVerification> = result.take(0)?;
-
-        Ok(email_verification.into_iter().next().unwrap())
+        Ok(EmailVerification::new(
+            email_verification_id,
+            email,
+            code,
+            created_at,
+            expires_at,
+        ))
     }
 
-    pub async fn remove(&self, email_verification_id: String) -> Result<(), surrealdb::Error> {
+    pub async fn remove(&self, email_verification_id: Thing) -> Result<(), surrealdb::Error> {
         let query = r#"
             DELETE FROM email_verification
             WHERE id = $email_verification_id
+            RETURN BEFORE
         "#;
 
-        let mut result: surrealdb::Response = self
+        let mut response: surrealdb::Response = self
             .db
             .query(query)
             .bind(("email_verification_id", email_verification_id))
             .await?;
 
-        let affected: Vec<EmailVerification> = result.take(0)?;
+        let result: Vec<EmailVerification> = response.take(0)?;
 
-        if affected.is_empty() {
+        // Check if the deletion affected any rows (result should be empty if nothing was deleted)
+        if result.is_empty() {
             return Err(surrealdb::Error::Api(
                 surrealdb::error::Api::InvalidRequest(String::from(
-                    "Email verification either doesn't exist or is already verified",
+                    "Email verification either doesn't exist or is already deleted",
                 )),
             ));
         }
