fabf513de56d1b34c7bc83970db7a5a02ab76d81
dawidseipold <seipold.dawid@gmail.com>
2024-11-19 05:31:16 +0100

    fix(utils): fix minor and major mistakes in the existing schemas

--- src/utils/schemas.rs
+++ src/utils/schemas.rs
@@ -2,7 +2,7 @@ pub const USER_SCHEMA: &str = r#"
     DEFINE TABLE user SCHEMAFULL;
 
     DEFINE FIELD email ON TABLE user TYPE string;
-    DEFINE FIELD email_verified ON TABLE user TYPE bool;
+    DEFINE FIELD email_verified ON TABLE user TYPE bool DEFAULT false;
     DEFINE FIELD password_hash ON TABLE user TYPE string;
     DEFINE FIELD created_at ON TABLE user TYPE datetime;
 "#;
@@ -12,7 +12,9 @@ pub const EMAIL_VERIFICATION_SCHEMA: &str = r#"
 
     DEFINE FIELD code ON TABLE email_verification TYPE string;
     DEFINE FIELD created_at ON TABLE email_verification TYPE datetime;
-    DEFINE FIELD expired_at ON TABLE email_verification TYPE datetime;
+    DEFINE FIELD expires_at ON TABLE email_verification TYPE datetime;
+
+    DEFINE FIELD user ON TABLE email_verification TYPE record<user>;
 "#;
 
 pub const SESSION_SCHEMA: &str = r#"
@@ -20,8 +22,10 @@ pub const SESSION_SCHEMA: &str = r#"
 
     DEFINE FIELD authorized ON TABLE session TYPE bool ;
     DEFINE FIELD created_at ON TABLE session TYPE datetime;
-    DEFINE FIELD expired_at ON TABLE session TYPE datetime;
-    DEFINE FIELD last_accessed_at_at ON TABLE session TYPE datetime;
+    DEFINE FIELD expires_at ON TABLE session TYPE datetime;
+    DEFINE FIELD last_accessed_at ON TABLE session TYPE datetime;
+
+    DEFINE FIELD user ON TABLE session TYPE record<user>;
 "#;
 
 // TODO: Create schemas for relation tables
