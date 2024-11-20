e3c3ff55d444f593f2bf196438249e38e97baa29
dawidseipold <seipold.dawid@gmail.com>
2024-11-20 05:36:38 +0100

    feat(utils): add password_reset_request schema

--- src/utils/schemas.rs
+++ src/utils/schemas.rs
@@ -20,12 +20,21 @@ pub const EMAIL_VERIFICATION_SCHEMA: &str = r#"
 pub const SESSION_SCHEMA: &str = r#"
     DEFINE TABLE session SCHEMAFULL;
 
-    DEFINE FIELD authorized ON TABLE session TYPE bool ;
+    DEFINE FIELD authorized ON TABLE session TYPE bool;
     DEFINE FIELD created_at ON TABLE session TYPE datetime;
     DEFINE FIELD expires_at ON TABLE session TYPE datetime;
     DEFINE FIELD last_accessed_at ON TABLE session TYPE datetime;
 
     DEFINE FIELD user ON TABLE session TYPE record<user>;
+"#;
+
+pub const PASSWORD_RESET_REQUEST_SCHEMA: &str = r#"
+    DEFINE TABLE password_reset_request SCHEMAFULL;
+
+    DEFINE FIELD created_at ON TABLE password_reset_request TYPE datetime;
+    DEFINE FIELD expires_at ON TABLE password_reset_request TYPE datetime;
+
+    DEFINE FIELD user ON TABLE password_reset_request TYPE record<user>;
 "#;
 
 // TODO: Create schemas for relation tables
